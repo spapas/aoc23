8d03b4a6a1ec36a24616ae0348f5e8df169f1596
Serafeim Papastefanos <spapas@gmail.com>
2024-05-29 13:43:55 +0300

    ok

--- day1.py
+++ day1.py
@@ -1,7 +1,21 @@
 with open("day1.txt") as fin:
     lines = fin.readlines()
 
+
 def get_calibration(line):
+    print(line)
+    line = (
+        line.replace("one", "one1one")
+        .replace("two", "two2two")
+        .replace("three", "three3three")
+        .replace("four", "four4four")
+        .replace("five", "five5five")
+        .replace("six", "six6six")
+        .replace("seven", "seven7seven")
+        .replace("eight", "eight8eight")
+        .replace("nine", "nine9nine")
+    )
+    print(line)
     digits = [int(d) for d in list(line) if d.isdigit()]
     if len(digits) == 1:
         return digits[0] * 10 + digits[0]
